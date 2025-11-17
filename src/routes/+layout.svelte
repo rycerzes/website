6f7787d3648aae2c96610bf49740668e1105478f
swappy <59965507+rycerzes@users.noreply.github.com>
2025-11-17 23:14:15 +0530

    inconsolata font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,9 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+	<link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200..900&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
 </svelte:head>
 
 <div class="min-h-screen bg-white text-gray-900 dark:bg-gray-900 dark:text-gray-100">
